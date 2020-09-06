334081b5cb833a9b12416518b76c83c4ec79cefe
Евгений <geshachapik@gmail.com>
2020-09-06 19:34:48 +0300

    add if statement for break

--- src/main/java/chaplinskiy/task1/Main.java
+++ src/main/java/chaplinskiy/task1/Main.java
@@ -9,29 +9,36 @@ public class Main {
         final AtomicBoolean atomicBooleanFirst = new AtomicBoolean(true);
         final AtomicBoolean atomicBooleanSecond = new AtomicBoolean(false);
         final AtomicBoolean atomicBooleanThird = new AtomicBoolean(false);
+        MyThread b = new MyThread("B", TypeMethod.B, foo);
+        MyThread a = new MyThread("A", TypeMethod.A, foo);
+        MyThread c = new MyThread("C", TypeMethod.C, foo);
 
-        if (atomicBooleanFirst.get()){
-            MyThread a = new MyThread("A", TypeMethod.A, foo);
-            a.start();
-            a.join();
-            atomicBooleanFirst.set(false);
-            atomicBooleanSecond.set(true);
+        while (true){
+            if (atomicBooleanSecond.get()){
+                b.start();
+                b.join();
+                atomicBooleanSecond.set(false);
+                atomicBooleanThird.set(true);
+            }
 
+            if (atomicBooleanThird.get()) {
+                c.start();
+                c.join();
+                atomicBooleanThird.set(false);
+            }
+
+            if (atomicBooleanFirst.get()){
+                a.start();
+                a.join();
+                atomicBooleanFirst.set(false);
+                atomicBooleanSecond.set(true);
+
+            }
+
+            if (!atomicBooleanFirst.get() && !atomicBooleanSecond.get() && !atomicBooleanThird.get()){
+                break;
+            }
         }
 
-        if (atomicBooleanSecond.get()){
-            MyThread b = new MyThread("B", TypeMethod.B, foo);
-            b.start();
-            b.join();
-            atomicBooleanSecond.set(false);
-            atomicBooleanThird.set(true);
-        }
-
-        if (atomicBooleanThird.get()) {
-            MyThread c = new MyThread("C", TypeMethod.C, foo);
-            c.start();
-            c.join();
-            atomicBooleanThird.set(false);
-        }
     }
 }
